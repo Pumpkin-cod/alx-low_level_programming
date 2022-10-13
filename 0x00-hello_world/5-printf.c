1954eff1b60803ede51a4f4266313189262cf8a6
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-13 12:21:05 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*Description: main - Prints out a string.
+*Return: 0 if successful.
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
