04e48b9ca17344d605d2b22fe2b81de132166a5e
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-13 12:14:31 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - This Prints out a string.
-*Return - 0 if successful.
+*Description: main - This Prints out a string.
+*Return: 0 if successful.
 */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
