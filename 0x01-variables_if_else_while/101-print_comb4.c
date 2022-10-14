db8e662681b80652e24d792aa7333421f740e5ac
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-14 16:43:45 +0000

    Combining three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int a, i, x;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (x = '0'; x <= '9'; x++)
+			{
+				if (a < i && i < x)
+				{
+					putchar(a);
+					putchar(i);
+					putchar(x);
+
+					if (a != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
