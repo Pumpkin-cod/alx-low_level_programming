26daab7abff88c9698373e01aaa21755dd66bc34
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-14 16:19:54 +0000

    Numbers with commas and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - Prints numbers with commas
+  *
+  * Return: 0 (Success);
+  */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
