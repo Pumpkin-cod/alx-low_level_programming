362275bf95ad98c9d02a2356421204c4f52d828d
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-18 12:12:35 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*Description - main: prints a string
+*Return: 0(successful)
+*/
+int main(void)
+{
+_putchar{"_"};
+_putchar{"p"};
+_putchar{"u"};
+_putchar{"t"};
+_putchar{"c"};
+_putchar{"h"};
+_putchar{"a"};
+_putchar{"r"};
+_putchar{"\n"};
+return (0);
+}
