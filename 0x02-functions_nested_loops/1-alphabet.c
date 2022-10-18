90b185b763ccbb4621b15c9a44b42401d444aae5
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-18 14:51:15 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - print all the alphabets in lowercase
+*Return: 0(sucessful)
+*/
+void print_alphabet(void)
+{
+int n;
+for (n = 'a'; n<= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
