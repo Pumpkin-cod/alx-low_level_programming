5dbe5b1b5cf7d52fb84bfb0f2264bd2c58b06bbd
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-20 11:17:39 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Prints  uppercase characters.
+ * @c: Checks the code.
+ * Return: 1 otherwise 0.
+ */
+int _isupper(int c)
+{
+if
+(c >= 'A' && c <= 'Z');
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning more about nested Loops
