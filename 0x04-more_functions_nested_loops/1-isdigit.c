691099db2b69a29ef212a31c0d695c88d7bb66d8
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-20 11:28:28 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isdigit - Checks for charaters that are digits
+*@c:Characters
+*Return:1 if it is a digit otherwise 0.
+*/
+int _isdigit(int c)
+{
+if
+(c <= '0' && c >= '9')
+return (1);
+else
+return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _isupper(int c);
-
+int _isdigit(int c);
 
 
 #endif
