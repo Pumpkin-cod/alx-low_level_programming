8b298cfe0d3d1da879db31e0d8b3fbb68bc04c04
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-21 10:49:44 +0000

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - Prints a square using #.
+ * @size: The size of the square.
+ */
+void print_square(int size)
+{
+int height, width;
+if (size > 0)
+{
+for (height = 0; height < size; height++)
+{
+for (width = 0; width < size; width++)
+_putchar('#');
+if (height == size - 1)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
