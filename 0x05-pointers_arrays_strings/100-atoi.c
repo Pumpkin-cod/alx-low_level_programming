5c192e8563f44691b3632032b30ca84fd079dea0
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-24 22:01:47 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,6 +7,39 @@
  */
 int _atoi(char *s)
 {
+	int a, b, c, d, e, f;
 
-	return (0);
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	e = 0;
+	f = 0;
+
+	while (s[d] != '\0')
+		d++;
+
+	while (a < d && e == 0)
+	{
+		if (s[a] == '-')
+			++b;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			f = s[a] - '0';
+			if (b % 2)
+				f = -f;
+			c = c * 10 + f;
+			e = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			e = 0;
+		}
+		a++;
+	}
+
+	if (e == 0)
+		return (0);
+
+	return (c);
 }
