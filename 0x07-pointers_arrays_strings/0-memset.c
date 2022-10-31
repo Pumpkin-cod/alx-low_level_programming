967663e99439ef04f6ab6cd6b0251e4cd708dbf5
Catherine Okang-Boye <katieboye44@gmail.com>
2022-10-31 14:32:00 +0000

    1-memcpy.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,10 +4,9 @@
  *_memset - fills memory with a constant byte n amount of times
  *
  *@s: pointer that has the address of the memory to fill
- *@b: constant byte that will be written into the momory
+ *@b: constant byte
  *@n: amount of times the byte will be written into the memory
- *
- *Return: address of the memory where thebyte was written
+ *Return: pointer to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -15,7 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
+	s[i] = b;
 	}
 	return (s);
 }
