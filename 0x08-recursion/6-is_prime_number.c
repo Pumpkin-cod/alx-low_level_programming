8bdfa7e864d0c7467e6842c8cccd0e4523119cfc
Catherine Okang-Boye <katieboye44@gmail.com>
2022-11-02 23:41:59 +0000

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+  * _isitprime - the prime
+  * @n: the number
+  * @i: the number / 2
+  *
+  * Return: 1 if prime, 0 if not
+  */
+int _isitprime(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (n % i == 0)
+			return (0);
+		else
+			return (_isitprime(n, i - 1));
+	}
+}
+/**
+ * is_prime_number - the prime
+ * @n: the number
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	if (n % 2 == 0)
+		return (0);
+	else
+		return (_isitprime(n, n / 2));
+
+}
